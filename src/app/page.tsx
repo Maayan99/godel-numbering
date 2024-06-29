0233994227c78ff619f2cba1e2cd5a052947d046
Maayan <maayansonnenfeld@gmail.com>
2024-06-30 00:05:57 +0300

    Trying out a fix to another import error

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 'use client'
 
 import Image from "next/image";
-import GodelNumberingVisualizer from "@/components/godel-numbering-visualizer";
+import GodelNumberingVisualizer from "../components/godel-numbering-visualizer";
 
 export default function Home() {
   return (
